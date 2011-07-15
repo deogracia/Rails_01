95fc165f0249eb943fbc196fc05175bd7f2b0170
Lionel FELICITE <deogracia@free.fr>
2011-07-16 00:15:40 +0200

    ajout dossier rails complet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails01::Application.config.secret_token = '454d34e6ea57486d39988024ca57c712feea5c2f08a4f84e3204488dc4092129320f99a246364865991d88491b2be04cd0a1b96164b6d4eeb5a489e51df7063c'
