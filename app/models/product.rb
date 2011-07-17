a420cbffbfb4b05c5324210de6105d3873971b38
Lionel FELICITE <deogracia@free.fr>
2011-07-17 18:36:13 +0200

    oubli lors du precedant commit ...

---
+++ app/models/product.rb
@@ -0,0 +1,14 @@
+class Product < ActiveRecord::Base
+  ############## validation ####################
+  #on verifie que les champs sont remplis
+  validates :title, :description, :image_url, :presence => true
+  #on verifie que le prix n'est pas nul
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  #on verifie que le titre est unoque
+  validates :title, :uniqueness => true
+  #on verifie l'extension de l'image
+  validates :image_url, :format => {
+      :with    => %r{\.(gif|jpg|png)}i,
+      :message => 'must be an URL for GIF, JPG or PNG image.'
+  }
+end
